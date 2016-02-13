2f1acb1a9abec2050283383a03d46d91a8e79eac
todorjelev36@gmail.com <todorjelev36@gmail.com>
2016-02-13 16:55:23 +0200

    - returning some comments

--- RaySphereTest/Sphere.cpp
+++ RaySphereTest/Sphere.cpp
@@ -55,6 +55,7 @@ bool Sphere::GetIntersection(Ray* ray, IntersectionInfo& info)
 	float b = DotProduct(m, ray->m_direction);
 	float c = DotProduct(m, m) - m_radius * m_radius;
 
+	// Exit if r’s origin outside s (c > 0) and r pointing away from s (b > 0)
 	if (c > 0.0f && b > 0.0f)
 	{
 		return false;
@@ -62,13 +63,16 @@ bool Sphere::GetIntersection(Ray* ray, IntersectionInfo& info)
 
 	float discr = b*b - c;
 
+	// A negative discriminant corresponds to ray missing sphere
 	if (discr < 0.0f)
 	{
 		return false;
 	}
 
+	// Ray now found to intersect sphere, compute smallest t value of intersection
 	float t = -b - sqrt(discr);
 
+	// If t is negative, ray started inside sphere so clamp t to zero
 	if (t < 0.0f) t = 0.0f;
 	info.m_intersPoint = ray->m_startPos + t * ray->m_direction;
 	info.m_object = this;
